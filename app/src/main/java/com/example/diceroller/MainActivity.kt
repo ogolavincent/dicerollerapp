df71fd9d27607af11333d6f7c7e725bdae4545ea
ogolavincent <shakilvincent100@gmail.com>
2024-02-13 07:54:27 +0300

    ongoing project

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+import kotlin.random.Random as Random
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+    }
+
+    private fun rollDice() {
+
+        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
+
+
+    }
+
+
+}
